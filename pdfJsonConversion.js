e1a51cd2419681f7e12d27e72c8445cb8ace338d
jbaranec <jozef.baranec@gmail.com>
2024-09-09 10:35:13 +0200

    new pdfJsonConversion.js example

---
+++ pdfJsonConversion.js
@@ -0,0 +1,40 @@
+const Pdfix = require('pdfix-sdk');
+
+const pdfixSdkWrapper = new Pdfix();
+pdfixSdkWrapper.loadPdfixSdk().then(() => {
+  const pdfixSdk = pdfixSdkWrapper.getPdfixSdk();
+  const pdfix = pdfixSdk.GetPdfix();
+  const pdfDoc = pdfixSdkWrapper.openDocumentFromPath("./pdf/test.pdf");
+
+  console.log({
+    numOfPages: pdfDoc.GetNumPages(),
+    version: pdfDoc.GetVersion(),
+    pdfStandard: pdfDoc.GetPdfStandard()
+  });
+
+  const jsonConversion = pdfDoc.CreateJsonConversion();
+
+  // prepare conversion params
+  const params = new pdfixSdk.PdfJsonParams();
+  params.struct_tree = 1; // export structure tree
+  params.text = 1; // include text
+  jsonConversion.SetParams(params);
+
+  // save JSON to memory stream
+  const stream = pdfix.CreateMemStream();
+  jsonConversion.SaveToStream(stream);
+  jsonConversion.Destroy()
+
+  // save data from stream to buffer
+  const buffer = new ArrayBuffer(stream.GetSize())
+  stream.ReadToArrayBuffer(0, buffer, buffer.byteLength)
+  stream.Destroy()
+
+  // display JSON
+  const decoder = new TextDecoder();
+  const str = decoder.decode(buffer);
+  console.log(str)
+
+  pdfDoc.Close();
+  pdfix.Destroy();
+});
